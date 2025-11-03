f25aff92c40185cbe13be52961ebd9331417e417
codingmaster094 <codingmaster094@gmail.com>
2025-11-03 10:03:40 +0530

    solved error

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -4,7 +4,6 @@ import Header from "../(frontend)/header/page"; // <-- PROBLEM IS HERE
 import "../../../public/css/globals.css";
 import Footer from "../(frontend)/footer/page";
 import Cookie from "./components/Cookie";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
